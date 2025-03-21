e08f95301ea9526909b527bd4611e70ed84d0475
avklimova <sav_rhome@mail.ru>
2025-03-21 20:31:16 +0300

    counter

--- 1.2.Components/src/app/counter2/counter2.component.ts
+++ 1.2.Components/src/app/counter2/counter2.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter2',
   templateUrl: './counter2.component.html',
   styleUrls: ['./counter2.component.css']
 })
-export class Counter2Component {
-
+export class Counter2Component implements AfterViewInit {
+  text = "SecondEvoApp";
+  
+  ngAfterViewInit(): void {
+      setInterval(() => {
+      this.text = this.text === "SecondEvoApp" ? "second-component works!" : "SecondEvoApp";
+    }, 2000);
+  }
 }
