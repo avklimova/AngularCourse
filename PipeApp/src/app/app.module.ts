086d2c07ec7f086c6252ad600bfc887e9b0debb7
avklimova <sav_rhome@mail.ru>
2025-03-21 21:02:33 +0300

    pipe

---
+++ PipeApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SumPipe } from './sum.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SumPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
